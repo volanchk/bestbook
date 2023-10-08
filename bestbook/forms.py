f01f3cadcf1f0d9ba58f52652ac9f99a84215e7e
volanchk <volanchk@gmail.com>
2023-10-08 19:30:46 +0300

    I've succesfully created viable registration form.

--- bestbook/forms.py
+++ bestbook/forms.py
@@ -9,4 +9,29 @@ class SignUserIn(forms.BaseForm):
 
 
 class RegisterForm(forms.Form):
-    pass
+    username = forms.CharField(max_length=100)
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "id": "user_password"
+            }
+        )
+    )
+    password2 = forms.CharField(
+        label="Confirmation",
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "id": "password_confirmation"
+            }
+        )
+    )
+
+    def clean_username(self):
+        username = self.cleaned_data.get("username")
+        qs = User.objects.filter(username__iexact=username)
+        if qs.exists():
+            raise forms.ValidationError("Create another name, please!")
+        return username
--- bestbook/views.py
+++ bestbook/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, get_user_model
+from django.contrib.auth.forms import User
 from .forms import RegisterForm
 
 
@@ -8,7 +9,27 @@ def home(request):
 
 
 def register(request):
-    return render(request, "register.html")
+    form = RegisterForm(request.POST or None)
+
+    if form.is_valid():
+
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password1")
+
+        try:
+            User.objects.create_user(username=username,
+                                     password=password)
+        finally:
+            user = None
+
+        user = authenticate(request, username=username,
+                            password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("/")
+
+    return render(request, "register.html", {"form": form})
 
 
 def log_in(request):
