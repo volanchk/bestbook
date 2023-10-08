7b99f9c93498330c914ccac41a7fb0809622e480
volanchk <volanchk@gmail.com>
2023-10-08 18:02:34 +0300

    It is the starting point for RegForm branch.

--- bestbook/views.py
+++ bestbook/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, get_user_model
-# from .forms import RegisterForm
+from .forms import RegisterForm
 
 
 def home(request):
