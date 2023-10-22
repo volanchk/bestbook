5f03039d295f4aaed4b27753340efa87bb8abcb9
volanchk <volanchk@gmail.com>
2023-10-22 21:41:38 +0300

    Minor bug fix.

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 from django.shortcuts import render, redirect, reverse
 from .forms import BooksForm, TopicsForm
 from .models import Topics, Books
@@ -46,13 +47,31 @@ def election(request, pk):
     books = [i['name'] for i in Books.objects.values('name').filter(topic=pk)]
     score = [i['votes'] for i in Books.objects.values('votes').filter(topic=pk)]
 
+    plt.style.use('Solarize_Light2')
     fig = Figure()
     ax = fig.subplots()
-    ax.bar(books, score, color="coral")
-
+    votes = ax.bar(books, score, color="coral",
+                   width=0.1, align="center",
+                   edgecolor="black", linewidth=0,
+                   label=topic_name)
+    ax.bar_label(votes)
+    ax.set_title(f'{topic_name}')
     plot = f"static/scores/{topic_name}.png"
 
     fig.savefig(plot)
+
+    picture_existence = bool
+
+    try:
+        picture_existence = open(plot, 'r')
+
+    except FileNotFoundError:
+
+        picture_existence = False
+
+    finally:
+
+        print(picture_existence)
 
     if request.POST.get('choice'):
         result = request.POST.get('choice')
@@ -65,7 +84,8 @@ def election(request, pk):
         "form": form,
         "topic": pk,
         "books_list": books_queryset,
-        "pic": plot
+        "pic": plot,
+        "picture_state": picture_existence
     }
 
     return render(request, "election.html", context)
