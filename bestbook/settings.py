f0de298b59cc201f827019c73f2b302ad0d8fb9b
volanchk <volanchk@gmail.com>
2023-10-25 23:12:09 +0300

    Almost working version.

--- bestbook/settings.py
+++ bestbook/settings.py
@@ -69,7 +69,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "bestbook.wsgi.application"
-
+ASGI_APPLICATION = "bestbook.asgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
